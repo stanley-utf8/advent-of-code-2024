5e0e4d21744da354bbbfd92f8ba477ffeda67ff6
twinfantasyfan <wangstanley910@gmail.com>
2024-12-06 15:05:38 -0500

    implement optimized part 2 -- Original solution time: 7.128167ms Optimized solution time: 5.388083ms

--- src/bin/day-02.rs
+++ src/bin/day-02.rs
@@ -3,17 +3,45 @@ use std::i32;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::path::Path;
+use std::time::Instant;
 
 fn main() -> std::io::Result<()> {
     let path = Path::new("./inputs/day-02.txt");
 
-    let res_01: i32 = part_01(path)?;
+    //let res_01: i32 = part_01(path)?;
+    //
+    //println!("levels safe: {}", res_01);
+    //
+    //let res_02: u32 = part_02(path);
+    //
+    //println!("levels safe: {}", res_02);
+    //
+    //let res_02_opt: u32 = part_02_opt(path);
+    //
+    //println!("levels safe: {}", res_02_opt);
 
-    println!("levels safe: {}", res_01);
+    let start = Instant::now();
+    let correct_results = part_02(path);
+    let duration = start.elapsed();
 
-    let res_02: u32 = part_02(path);
+    println!("Original solution time: {:?}", duration);
 
-    println!("levels safe: {}", res_02);
+    let start = Instant::now();
+    let opt_results = part_02_opt(path);
+    let duration = start.elapsed();
+
+    println!("Optimized solution time: {:?}", duration);
+
+    println!("Sequences found in correct but not in optimized:");
+
+    for sequence in correct_results.iter() {
+        if !opt_results.contains(sequence) {
+            println!("{}", sequence);
+        }
+    }
+
+    println!("\nCorrect count: {}", correct_results.len());
+    println!("Optimized count: {}", opt_results.len());
 
     Ok(())
 }
@@ -71,13 +99,13 @@ fn check_line(nums: &[u32], direction: bool) -> bool {
     nums.windows(2).all(|n| check_pair(n[0], n[1], direction))
 }
 
-fn part_02(path: &Path) -> u32 {
+fn part_02(path: &Path) -> Vec<String> {
     let f = File::open(&path).expect("failed to open file");
 
     let reader = BufReader::new(f);
     reader
         .lines()
-        .filter(|line| {
+        .filter_map(|line| {
             //predicate + &pattern matches on the reference to line
             if let Ok(line) = line {
                 let nums = line
@@ -87,23 +115,13 @@ fn part_02(path: &Path) -> u32 {
                 let direction = nums[0] <= nums[1];
 
                 if check_line(&nums, direction) {
-                    return true;
-                }
-
-                if let Some(problem_index) = nums
-                    .windows(2)
-                    .enumerate()
-                    .find(|(_idx, pair)| !check_pair(pair[0], pair[1], direction))
-                    .map(|(idx, _pair)| idx)
-                // return just index of first problem pair
-                {
-                    //test removes around problem level
+                    return Some(line);
                 } else {
                     // edge case: remove index 0
                     let direction_0 = nums[1] <= nums[2];
 
                     if check_line(&nums[1..], direction_0) {
-                        return true;
+                        return Some(line);
                     }
 
                     // edge case: remove index 1
@@ -112,12 +130,12 @@ fn part_02(path: &Path) -> u32 {
                         && nums[0].abs_diff(nums[2]) <= 3
                         && check_line(&nums[2..], direction_1)
                     {
-                        return true;
+                        return Some(line);
                     }
 
                     // edge case: remove last index
                     if check_line(&nums[..nums.len() - 1], direction) {
-                        return true;
+                        return Some(line);
                     }
 
                     // rest of cases:
@@ -126,14 +144,64 @@ fn part_02(path: &Path) -> u32 {
                             && check_pair(nums[removed - 1], nums[removed + 1], direction)
                             && check_line(&nums[removed + 1..], direction)
                         {
-                            return true;
+                            return Some(line);
                         }
                     }
-                    return false;
+                    return None;
                 }
             } else {
-                return false;
+                return None;
             }
         })
-        .count() as u32
+        .collect()
+}
+
+fn part_02_opt(path: &Path) -> Vec<String> {
+    let f = File::open(&path).expect("failed to open file");
+
+    let reader = BufReader::new(f);
+    reader
+        .lines()
+        .filter_map(|line| {
+            if let Ok(line) = line {
+                let nums = line
+                    .split_whitespace()
+                    .map(|str_num| str_num.parse::<u32>().unwrap())
+                    .collect::<Vec<u32>>();
+                let direction = nums[0] <= nums[1];
+
+                if check_line(&nums, direction) {
+                    return Some(line);
+                }
+                if let Some((problem_idx, _)) = nums
+                    .windows(2)
+                    .enumerate()
+                    .find(|(_idx, pair)| !check_pair(pair[0], pair[1], direction))
+                {
+                    if problem_idx == 1 {
+                        let direction_without_first = nums[1] <= nums[2];
+                        if check_line(&nums[1..], direction_without_first) {
+                            return Some(line);
+                        }
+                    }
+                    let mut nums_without_first = nums.clone();
+                    nums_without_first.remove(problem_idx);
+                    let direction_first = nums_without_first[0] <= nums_without_first[1];
+                    if check_line(&nums_without_first, direction_first) {
+                        return Some(line);
+                    }
+
+                    let mut nums_without_second = nums.clone();
+                    nums_without_second.remove(problem_idx + 1);
+                    let direction_second = nums_without_second[0] <= nums_without_second[1];
+                    if check_line(&nums_without_second, direction_second) {
+                        return Some(line);
+                    }
+                }
+                None
+            } else {
+                None
+            }
+        })
+        .collect()
 }
